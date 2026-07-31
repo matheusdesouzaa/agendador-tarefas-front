a62452660700957c470baf1d214d9537bf34f9d2
Matheus de Souza <theu205@gmail.com>
2026-07-31 20:28:01 -0300

    create form logic

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,15 +5,32 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { PasswordFieldComponent } from '../../shared/components/password-field/password-field.component';
+import { ReactiveFormsModule,FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatSelectModule,PasswordFieldComponent],
+  imports: [MatCardModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatSelectModule,PasswordFieldComponent, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class RegisterComponent {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      password: ['']
+    })
+  }
+
+  get passwordControl(): FormControl{
+    return this.form.get('password') as FormControl;
+  }
+
+  submit(){
+    console.log(this.form.value)
+  }
 }
--- src/app/shared/components/password-field/password-field.component.ts
+++ src/app/shared/components/password-field/password-field.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, signal} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -6,12 +7,15 @@ import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'input-password-field',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule,ReactiveFormsModule],
   templateUrl: './password-field.component.html',
   styleUrl: './password-field.component.scss'
 })
 export class PasswordFieldComponent {
 hide = signal(true);
+
+  @Input({required:true}) control!: FormControl;
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
