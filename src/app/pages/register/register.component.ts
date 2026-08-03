216cd6a2c4d66bb894c97d12830f2d9b0013bb99
Matheus de Souza <theu205@gmail.com>
2026-08-03 15:29:59 -0300

    add form validators

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,7 +5,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { PasswordFieldComponent } from '../../shared/components/password-field/password-field.component';
-import { ReactiveFormsModule,FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule,FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -20,8 +20,8 @@ export class RegisterComponent {
 
   constructor(private formBuilder: FormBuilder){
     this.form = this.formBuilder.group({
-      fullName: [''],
-      email: [''],
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['']
     })
   }
@@ -31,6 +31,10 @@ export class RegisterComponent {
   }
 
   submit(){
-    console.log(this.form.value)
+    if(this.form.invalid){
+       this.form.markAllAsTouched();
+       return;
+    }
+    console.log("formulário submetido",this.form.value)
   }
 }
