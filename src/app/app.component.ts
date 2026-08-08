1e1492bdf5003e6df5d3530d06a11a9f19ccf5cf
Matheus de Souza <theu205@gmail.com>
2026-08-07 23:59:16 -0300

    create home and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenuComponent } from './shared/components/global/top-menu/top-menu.component';
+import { FooterComponent } from './shared/components/global/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopMenuComponent],
+  imports: [RouterOutlet, TopMenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
